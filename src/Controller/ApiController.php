04e614fe0c41d40bb15a3366dc0199a6632f3697
Romain DUGAS <RDUGAS>
2022-10-12 08:33:34 +0200

    API : Ajout d'une série, Suppression d'une série, Modification complète d'une série

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -7,10 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\PdoFouDeSerie;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class ApiController extends AbstractController
 {
-    #[Route('/api/series', name: 'app_api')]
+    #[Route('/api/series', name: 'app_api_series', methods: ['GET'])]
     public function getListeSeries(PdoFouDeSerie $pdoFouDeSerie): Response
     {
         $lesSeries = $pdoFouDeSerie->getLesSeries();
@@ -28,7 +29,7 @@ class ApiController extends AbstractController
         return new JsonResponse($tableau);
     }
 
-    #[Route('/api/series/{id}', name: 'app_api')]
+    #[Route('/api/series/{id}', name: 'app_api_serie_id', methods: ['GET'])]
     public function getLaSerie(PdoFouDeSerie $pdoFouDeSerie, $id): Response
     {
         $laSerie = $pdoFouDeSerie->getLaSerie($id);
@@ -42,6 +43,59 @@ class ApiController extends AbstractController
                 'image'=>$laSerie['image']
             ];
             return new JsonResponse($tableau);
+        } else {
+            return new JsonResponse(['message' => 'Serie not found'], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    #[Route('/api/series', name: 'app_api_newSerie', methods: ['POST'])]
+    public function newSerie(Request $request, PdoFouDeSerie $pdoFouDeSerie)
+    {
+        $content = $request->getContent();
+        if (!empty($content)) {
+            $laSerie = json_decode($content, true);
+            $laSerieAjoutee = $pdoFouDeSerie->setLaSerie($laSerie);
+            $tableau = [
+                'id'=>$laSerieAjoutee['id'],
+                'titre'=>$laSerieAjoutee['titre'],
+                'resume'=>$laSerieAjoutee['resume'],
+                'duree'=>$laSerieAjoutee['duree'],
+                'premiereDiffusion'=>$laSerieAjoutee['premiereDiffusion'],
+                'image'=>$laSerieAjoutee['image']
+            ];
+        }
+
+        return new JsonResponse($tableau, Response::HTTP_CREATED);
+    }
+
+    #[Route('/api/series/{id}', name: 'app_api_deleteSerie_id', methods: ['DELETE'])]
+    public function deleteSerie($id, PdoFouDeSerie $pdoFouDeSerie) {
+        $laSerie = $pdoFouDeSerie->getLaSerie($id);
+        if ($laSerie !== false) {
+            $pdoFouDeSerie->deleteLaSerie($id);
+
+            return new JsonResponse('La ressource a été supprimé', Response::HTTP_NO_CONTENT);
+        } else {
+            return new JsonResponse(['message' => 'Serie not found'], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    #[Route('/api/series/{id}', name: 'app_api_updateSerie_id', methods: ['PUT'])]
+    public function updateSerieComplete(Request $request, $id, PdoFouDeSerie $pdoFouDeSerie) {
+        $laSerie = $pdoFouDeSerie->getLaSerie($id);
+        $content = $request->getContent();
+        if ($laSerie !== false) {
+            $laSerie = json_decode($content, true);
+            $laSerieModifiee = $pdoFouDeSerie->updateLaSerie($laSerie, $id);
+            $tableau = [
+                'id'=>$laSerieModifiee['id'],
+                'titre'=>$laSerieModifiee['titre'],
+                'resume'=>$laSerieModifiee['resume'],
+                'duree'=>$laSerieModifiee['duree'],
+                'premiereDiffusion'=>$laSerieModifiee['premiereDiffusion'],
+                'image'=>$laSerieModifiee['image']
+            ];
+            return new JsonResponse($tableau, Response::HTTP_OK);
         } else {
             return new JsonResponse(['message' => 'Serie not found'], Response::HTTP_NOT_FOUND);
         }
--- src/Service/PdoFouDeSerie.php
+++ src/Service/PdoFouDeSerie.php
@@ -33,4 +33,42 @@ class PdoFouDeSerie
 		$leNombre = $req->fetch();
 		return $leNombre;
     }
+
+    public function setLaSerie($laSerie) {
+        $req = PdoFouDeSerie::$monPdo->prepare("INSERT INTO serie (titre, resume, duree, premiereDiffusion, image) VALUES (:titre, :resume, :duree, :premiereDiffusion, :image)");
+        $req->bindValue(':titre', $laSerie['titre'], PDO::PARAM_STR);
+        $req->bindValue(':resume', $laSerie['resume'], PDO::PARAM_STR);
+        $req->bindValue(':duree', $laSerie['duree'], PDO::PARAM_STR);
+        $req->bindValue(':premiereDiffusion', $laSerie['premiereDiffusion'], PDO::PARAM_STR);
+        $req->bindValue(':image', $laSerie['image'], PDO::PARAM_STR);
+		$req->execute();
+
+        $req2 = PdoFouDeSerie::$monPdo->prepare("SELECT * FROM serie WHERE id=last_insert_id()");
+        $req2->execute();
+		$laLigne = $req2->fetch();
+		return $laLigne;
+    }
+
+    public function deleteLaSerie($id) {
+        $req = PdoFouDeSerie::$monPdo->prepare("DELETE FROM serie WHERE id=:id");
+        $req->bindValue(':id', $id, PDO::PARAM_STR);
+        $req->execute();
+    }
+
+    public function updateLaSerie($laSerie, $id) {
+        $req = PdoFouDeSerie::$monPdo->prepare("UPDATE serie SET id=id, titre=:titre, resume=:resume, duree=:duree, premiereDiffusion=:premiereDiffusion, image=:image WHERE id=:id");
+        $req->bindValue(':id', $id, PDO::PARAM_STR);
+        $req->bindValue(':titre', $laSerie['titre'], PDO::PARAM_STR);
+        $req->bindValue(':resume', $laSerie['resume'], PDO::PARAM_STR);
+        $req->bindValue(':duree', $laSerie['duree'], PDO::PARAM_STR);
+        $req->bindValue(':premiereDiffusion', $laSerie['premiereDiffusion'], PDO::PARAM_STR);
+        $req->bindValue(':image', $laSerie['image'], PDO::PARAM_STR);
+        $req->execute();
+
+        $req2 = PdoFouDeSerie::$monPdo->prepare("SELECT * FROM serie WHERE id=:id");
+        $req2->bindValue(':id', $id, PDO::PARAM_INT);
+        $req2->execute();
+		$laLigne = $req2->fetch();
+		return $laLigne;
+    }
 }
