98949bf193d2fa66992813302c5ac07f1c2c481a
Remery <romain.dugas85@gmail.com>
2022-11-10 14:22:45 +0100

    a

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Serie;
+use App\Entity\Genre;
+use Doctrine\Persistence\ManagerRegistry;
 
 class GenreController extends AbstractController
 {
     #[Route('/testGenre', name: 'app_testGenre')]
-    public function testGenre(): Response
+    public function testGenre(ManagerRegistry $doctrine): Response
     {
-        return $this->render('genre/index.html.twig', [
-            'controller_name' => 'GenreController',
-        ]);
+        $genre = new Genre();
+        $genre->setlibelle('Policier');
+
+        $entityManager=$doctrine->getManager();
+        $entityManager->persist($genre);
+
+        $repository = $doctrine->getRepository(Serie::class);
+        $laSerie = $repository->find(1);
+        $laSerie->addGenre($genre);
+
+        $entityManager->flush();
+
+        return $this->render('genre/index.html.twig', ['serie' => $laSerie]);
     }
 }
